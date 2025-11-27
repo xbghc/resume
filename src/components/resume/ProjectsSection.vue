731c6a03274ac426ab0aa1decf1980ff679e0508
xbghc <1774733158@qq.com>
2025-11-27 21:50:02 +0800

    allow markdown

--- src/components/resume/ProjectsSection.vue
+++ src/components/resume/ProjectsSection.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useResumeStore } from '@/stores/resume'
+import { marked } from 'marked'
 
 const { t } = useI18n()
 const store = useResumeStore()
+
+marked.setOptions({ breaks: true })
+
+function renderMarkdown(text: string) {
+  return marked.parse(text) as string
+}
 </script>
 
 <template>
@@ -14,7 +21,7 @@ const store = useResumeStore()
     <div v-for="project in store.data.projects" :key="project.name" class="mb-2">
       <p class="font-semibold text-sm">{{ project.name }}</p>
       <p v-if="project.role" class="text-gray-500 text-xs">{{ project.role }}</p>
-      <p class="text-xs mt-0.5">{{ project.description }}</p>
+      <div class="text-xs mt-0.5 markdown-content" v-html="renderMarkdown(project.description)"></div>
     </div>
   </section>
 </template>
